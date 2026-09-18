0d4391156c41b12a6a68e6c320200c15a45b2d5a
mohdaie <ayiepts@gmail.com>
2026-09-18 22:50:44 +0800

    Bump RunSGD v2.3.0 service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='runsgd-shell-v2212';
+const CACHE='runsgd-shell-v2300';
 const SHELL=['/','/index.html','/manifest.webmanifest','/icon.svg','/privacy.html'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
