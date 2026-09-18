1bce9beee3ed36c54a0f0003c5eea059134f3a68
mohdaie <ayiepts@gmail.com>
2026-09-18 17:04:52 +0800

    RunSGD v2.2.8: refresh JB routing cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='runsgd-shell-v227';
+const CACHE='runsgd-shell-v228';
 const SHELL=['/','/index.html','/manifest.webmanifest','/icon.svg','/privacy.html'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
