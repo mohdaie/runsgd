075a340d568da5dba31999635f1e8fef427c37d5
mohdaie <ayiepts@gmail.com>
2026-09-19 15:45:09 +0800

    Refresh RunSGD shell cache for v2.5.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='runsgd-shell-v2500';
+const CACHE='runsgd-shell-v2510';
 const SHELL=['/','/index.html','/manifest.webmanifest','/icon.svg','/privacy.html'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
