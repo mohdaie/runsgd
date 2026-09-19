893278081abd2a3110d4e94902025fa1b853945e
mohdaie <ayiepts@gmail.com>
2026-09-19 14:30:23 +0800

    Refresh RunSGD shell cache for v2.4.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='runsgd-shell-v2300';
+const CACHE='runsgd-shell-v2420';
 const SHELL=['/','/index.html','/manifest.webmanifest','/icon.svg','/privacy.html'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
