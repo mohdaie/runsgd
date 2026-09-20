9199736aa1f3bbe4f955c6e5e227ab806d6c4799
mohdaie <ayiepts@gmail.com>
2026-09-20 09:01:00 +0800

    Refresh app shell cache for v2.11.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='runsgd-shell-v2109';
+const CACHE='runsgd-shell-v2110';
 const SHELL=['/','/index.html','/manifest.webmanifest','/icon.svg','/privacy.html'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
