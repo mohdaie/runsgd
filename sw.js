2b140fdacfc56763d20dc6f4c36b0df9bd87494a
mohdaie <ayiepts@gmail.com>
2026-09-20 21:20:14 +0800

    Refresh PWA cache for v2.13.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='runsgd-shell-v2132';
+const CACHE='runsgd-shell-v2133';
 const SHELL=['/','/index.html','/VERSION','/manifest.webmanifest','/icon.svg','/privacy.html','/admin/','/admin/index.html'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
