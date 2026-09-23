3ed3104229fa927dfdbcf096b0f4c86fab5e7494
mohdaie <ayiepts@gmail.com>
2026-09-23 11:49:30 +0800

    Bump service worker cache to 2.15.8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='runsgd-shell-v2157';
+const CACHE='runsgd-shell-v2158';
 const SHELL=['/','/index.html','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
